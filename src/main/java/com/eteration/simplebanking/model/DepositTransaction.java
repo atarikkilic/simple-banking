c76ce9f7a2209cb214f5ebf5e0a876261395632b
Tarik Kilic <tarikkilic07@gmail.com>
2024-12-01 18:36:29 +0300

    Add JPA annotations to domain models

--- src/main/java/com/eteration/simplebanking/model/DepositTransaction.java
+++ src/main/java/com/eteration/simplebanking/model/DepositTransaction.java
@@ -1,9 +1,18 @@
 package com.eteration.simplebanking.model;
 
+import javax.persistence.Entity;
+import javax.persistence.DiscriminatorValue;
+
+@Entity
+@DiscriminatorValue("DepositTransaction")
 public class DepositTransaction extends Transaction {
 
     public DepositTransaction(Double amount) {
         super(amount);
+    }
+
+    protected DepositTransaction() {
+        super();
     }
 
     @Override
--- src/main/java/com/eteration/simplebanking/model/WithdrawalTransaction.java
+++ src/main/java/com/eteration/simplebanking/model/WithdrawalTransaction.java
@@ -1,9 +1,18 @@
 package com.eteration.simplebanking.model;
 
+import javax.persistence.Entity;
+import javax.persistence.DiscriminatorValue;
+
+@Entity
+@DiscriminatorValue("WithdrawalTransaction")
 public class WithdrawalTransaction extends Transaction {
 
     public WithdrawalTransaction(Double amount) {
         super(amount);
+    }
+
+    protected WithdrawalTransaction() {
+        super();
     }
 
     @Override
