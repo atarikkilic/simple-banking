2921f4ae5110f992874ed560caf0e2bcfd8ed2f2
Tarik Kilic <tarikkilic07@gmail.com>
2024-12-01 15:49:03 +0300

    Implement core domain models

--- src/main/java/com/eteration/simplebanking/model/Account.java
+++ src/main/java/com/eteration/simplebanking/model/Account.java
@@ -1,7 +1,65 @@
 package com.eteration.simplebanking.model;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
 
-// This class is a place holder you can change the complete implementation
-
+@Entity
 public class Account {
+    @Id
+    private String accountNumber;
+
+    private String owner;
+    private Double balance;
+    private Date createDate;
+
+    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL)
+    private List<Transaction> transactions;
+
+    public Account(String owner, String accountNumber) {
+        this.owner = owner;
+        this.accountNumber = accountNumber;
+        this.balance = 0.0;
+        this.transactions = new ArrayList<>();
+        this.createDate = new Date();
+    }
+
+    protected Account() {} // For JPA
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void deposit(Double amount) {
+        this.balance += amount;
+    }
+
+    public void withdraw(Double amount) throws InsufficientBalanceException {
+        if (this.balance < amount) {
+            throw new InsufficientBalanceException();
+        }
+        this.balance -= amount;
+    }
+
+    public void post(Transaction transaction) throws InsufficientBalanceException {
+        transaction.execute(this);
+        transactions.add(transaction);
+    }
 }
--- src/main/java/com/eteration/simplebanking/model/Transaction.java
+++ src/main/java/com/eteration/simplebanking/model/Transaction.java
@@ -1,7 +1,46 @@
 package com.eteration.simplebanking.model;
 
+import java.util.Date;
+import java.util.UUID;
+import javax.persistence.*;
 
-// This class is a place holder you can change the complete implementation
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "type")
 public abstract class Transaction {
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Date date;
+    private Double amount;
+    private String approvalCode;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    public Transaction(Double amount) {
+        this.date = new Date();
+        this.amount = amount;
+        this.approvalCode = UUID.randomUUID().toString();
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public String getApprovalCode() {
+        return approvalCode;
+    }
+
+    public String getType() {
+        return this.getClass().getSimpleName();
+    }
+
+    public abstract void execute(Account account) throws InsufficientBalanceException;
 }
